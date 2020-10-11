b4dedbf05b42038ed8e826bfdadc1551ff7374a4
Michael Heinrichs <netopyr@users.noreply.github.com>
2020-10-11 08:53:45 +0200

    Initial commit

---
+++ src/main/java/ch/fhnw/webec/multiplicationcircle/MultiplicationCircleApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.webec.multiplicationcircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplicationCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplicationCircleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/fhnw/webec/multiplicationcircle/MultiplicationCircleApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.webec.multiplicationcircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplicationCircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
